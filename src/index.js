4c8f17b40530d7b51f213eb6402aac1b1b1a332d
Yureka Adhikari <adhikariyureka@gmail.com>
2024-10-10 19:25:50 +0545

    React-1

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <>
+    <h1> Hi! I am Yureka! </h1>
+    <p>Welcome to my page!</p>
+    <h2> The sum of 10+3 is {3 + 2 * 5}</h2>
+  </>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
